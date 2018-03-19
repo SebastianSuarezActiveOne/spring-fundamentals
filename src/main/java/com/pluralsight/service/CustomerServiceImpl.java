3bf373d374af058e871d71e1459ea75292c09be1
Sebastian Suarez <sebastian.suarez@activeone.co>
2018-03-19 00:42:28 -0500

    Changed to autowired contructor annotations

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -14,14 +14,16 @@ public class CustomerServiceImpl implements CustomerService {
     //@Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
     public CustomerServiceImpl(CustomerRepository customerRepository) {
+        System.out.println("Using constructor injection");
         this.customerRepository = customerRepository;
     }
 
     public CustomerServiceImpl() {
     }
 
-    @Autowired
+    //@Autowired
     public void setCustomerRepository(CustomerRepository customerRepository) {
         System.out.println("We are using setter injection");
         this.customerRepository = customerRepository;
