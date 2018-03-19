4cdf5717f154f6f854f24361ce4b7c0633a2ae44
Sebastian Suarez <sebastian.suarez@activeone.co>
2018-03-18 22:14:58 -0500

    Changed to contructor injection

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -11,6 +11,13 @@ public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
 
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public CustomerServiceImpl() {
+    }
+
     public void setCustomerRepository(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
